60456dc51be1168f65990102cb99ba4691997e80
Halil Hodzic <habetech@gmail.com>
2026-09-20 13:02:47 +0200

    sync: server/db.js

---
+++ server/db.js
@@ -0,0 +1,7 @@
+import pkg from 'pg';
+const { Pool } = pkg;
+
+export const pool = new Pool({
+  connectionString: process.env.DATABASE_URL,
+  ssl: process.env.DATABASE_URL && process.env.DATABASE_URL.includes('sslmode=require') ? { rejectUnauthorized: false } : false
+});
